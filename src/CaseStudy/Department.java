8f8f5de132d4c5f3d88a2fd131a3a1740d8ead18
Lerish-vZ <lerishavz@gmail.com>
2024-05-24 10:28:26 +0200

    update

--- src/CaseStudy/Department.java
+++ src/CaseStudy/Department.java
@@ -13,6 +13,10 @@ public class Department {
     
     private Employee[] emps = new Employee[10];
 
+    public Department(String name){
+        this.name = name;
+    }
+    
     public String getName() {
         return name;
     }
