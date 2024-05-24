f257940ca3b77120983cc13660327a28e703e4f2
Lerish-vZ <lerishavz@gmail.com>
2024-05-24 09:21:51 +0200

    first commit

---
+++ src/CaseStudy/Employee.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CaseStudy;
+
+/**
+ *
+ * @author lerishav
+ */
+public class Employee {
+
+    private int empId;
+    private String empName;
+    private double empSalary;
+
+    public Employee(int empId, String empName, double empSalary) {
+        this.empId = empId;
+        this.empName = empName;
+        this.empSalary = empSalary;
+    }
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public double getEmpSalary() {
+        return empSalary;
+    }
+
+    public void setEmpSalary(double empSalary) {
+        this.empSalary = empSalary;
+    }
+}
